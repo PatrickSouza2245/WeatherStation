03bf33d127f5a9366c073451c1d1501f9fd1ce08
Patrick Souza <159404108+PatrickSouza2245@users.noreply.github.com>
2025-01-24 22:03:03 -0300

    Adição de animação e finalizando estilizações. Foi adicionado um rodapé tambem.

--- src/components/WeatherCard.jsx
+++ src/components/WeatherCard.jsx
@@ -9,7 +9,8 @@ const WeatherCard = () => {
   
   const [city, setCity] = useState('')
   const [weather, setWeather] = useState(null)
-  
+  const [isVisible, setIsVisible] = useState(false)
+
   const API_KEY = import.meta.env.VITE_API_KEY
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&lang=pt_br&units=metric`
 
@@ -28,9 +29,10 @@ const WeatherCard = () => {
     try{
     const res = await axios.get(url)
     setWeather(res.data) 
-    console.log(res);
-       
     setCity('')
+    setIsVisible(false)
+
+    setTimeout(() => setIsVisible(true), 50)
     
     toast.success("Cidade encontrada com sucesso!", {
       position: "top-right",
@@ -40,13 +42,14 @@ const WeatherCard = () => {
       autoClose: 3000,
 })
     } catch (error) {
-      toast.error("Erro ao procurar a cidade, tente novamente." + error, {
+      toast.error("Erro ao procurar a cidade, tente novamente.", {
         position: "top-right",
         theme: "colored",
         hideProgressBar: true,
         pauseOnHover: false,
         autoClose: 3000,
   })
+  console.log(error)
   setCity("")
     }
   }
@@ -60,20 +63,22 @@ const WeatherCard = () => {
           <button className='btnSearch' onClick={handleSearch}><IoSearchOutline /></button>
         </div>
 
-        { weather &&
-        <div className='result-weather'>
+        { weather && (
+        <div className={`result-weather ${isVisible ? 'visible' : ''}`}>
           <p>{weather.name} - {weather.sys.country}</p>
           <p><b>Clima:</b> {weather.weather[0].description}</p>
           <p>{(weather.main.temp).toFixed(0)}°C</p>
-          <p>Min: {(weather.main.temp_min).toFixed(0)}</p>
-          <p>Max: {(weather.main.temp_max).toFixed(0)}</p>
+          <p><b>Min:</b> {(weather.main.temp_min).toFixed(0)}°C</p>
+          <p><b>Max:</b> {(weather.main.temp_max).toFixed(0)}°C</p>
           <div className='resultInfo'>
-            <p>Sensação Térmica: {(weather.main.feels_like).toFixed(0)}°C</p>
-            <p>Umidade: {weather.main.humidity}%</p>
-            <p>Pressão: {weather.main.pressure} hPa</p>
+            <p><b>Sensação Térmica:</b> {(weather.main.feels_like).toFixed(0)}°C</p>
+            <p><b>Umidade:</b> {weather.main.humidity}%</p>
+            <p><b>Pressão:</b> {weather.main.pressure} hPa</p>
           </div>
         </div>  
-        }
+        )}
+
+        <footer>&copy; 2025, Desenvolvido por Patrick Souza </footer>
     </div>
   )
 }
